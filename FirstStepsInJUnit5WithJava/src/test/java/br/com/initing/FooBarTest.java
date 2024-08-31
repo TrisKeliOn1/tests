e940622155c531cd0f94e243f7dde1b014bccc5c
Hebert <hebertLandim007@outlook.com>
2024-08-31 16:39:38 -0300

    Added Dependencies, Assertions and Automated Tests for Mathematical Operations

---
+++ FirstStepsInJUnit5WithJava/src/test/java/br/com/initing/FooBarTest.java
@@ -0,0 +1,16 @@
+package br.com.initing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FooBarTest {
+
+    @Test
+    void test() {
+        //fail("Not yet implemented");
+
+        System.out.println("Hello!");
+
+    }
+}
