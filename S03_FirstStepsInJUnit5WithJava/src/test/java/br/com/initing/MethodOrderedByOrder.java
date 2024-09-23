eb30cb2b145a7ed68fce42b8049e2e82d8101433
Hebert <hebertLandim007@outlook.com>
2024-09-23 14:54:34 -0300

    @TestMethodOrder Index ordering with @Order

---
+++ S03_FirstStepsInJUnit5WithJava/src/test/java/br/com/initing/MethodOrderedByOrder.java
@@ -0,0 +1,34 @@
+package br.com.initing;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MethodOrderedByOrder {
+
+    @Test
+    @Order(1)
+    void testC() {
+        System.out.println("Running test C");
+    }
+
+    @Test
+    @Order(2)
+    void testD() {
+        System.out.println("Running test D");
+    }
+
+    @Test
+    @Order(3)
+    void testA() {
+        System.out.println("Running test A");
+    }
+
+    @Test
+    @Order(4)
+    void testB() {
+        System.out.println("Running test B");
+    }
+}
