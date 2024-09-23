f7b93c2299f746f762d8f1ced849383f7f22688d
Hebert <hebertLandim007@outlook.com>
2024-09-23 16:13:58 -0300

    Defining the Execution Order of Test Classes with @Order

--- S03_FirstStepsInJUnit5WithJava/src/test/java/br/com/initing/MethodOrderedRandonlyTest.java
+++ S03_FirstStepsInJUnit5WithJava/src/test/java/br/com/initing/MethodOrderedRandonlyTest.java
@@ -1,9 +1,11 @@
 package br.com.initing;
 
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+@Order(1)
 @TestMethodOrder(MethodOrderer.Random.class)
 public class MethodOrderedRandonlyTest {
 
