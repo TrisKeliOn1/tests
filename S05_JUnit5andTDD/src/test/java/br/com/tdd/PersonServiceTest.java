40244bf48299fd2a68f34a8c4bf60c3578c46335
Hebert <hebertLandim007@outlook.com>
2024-09-24 17:04:36 -0300

    Refactoring Unit Tests

--- S05_JUnit5andTDD/src/test/java/br/com/tdd/PersonServiceTest.java
+++ S05_JUnit5andTDD/src/test/java/br/com/tdd/PersonServiceTest.java
@@ -3,6 +3,7 @@ package br.com.tdd;
 import br.com.tdd.model.Person;
 import br.com.tdd.service.IPersonService;
 import br.com.tdd.service.PersonService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,20 +11,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PersonServiceTest {
 
-    @DisplayName("When Create a Person with Success Should Return a Person Object")
-    @Test
-    void testCreatePerson_WhenSuccess_ShouldReturnPersonObject() {
+    Person person;
 
-        // Given
-        IPersonService service = new PersonService();
-
-        Person person = new Person(
+    @BeforeEach
+    void setup() {
+        person = new Person(
                 "Keith",
                 "Moon",
                 "kmoon@tdd.com.br",
                 "Wembley - UK",
                 "Male"
         );
+    }
+
+    @DisplayName("When Create a Person with Success Should Return a Person Object")
+    @Test
+    void testCreatePerson_WhenSuccess_ShouldReturnPersonObject() {
+
+        // Given
+        IPersonService service = new PersonService();
 
         // When
         Person actual = service.createPerson(person);
@@ -38,14 +44,6 @@ public class PersonServiceTest {
 
         // Given
         IPersonService service = new PersonService();
-
-        Person person = new Person(
-                "Keith",
-                "Moon",
-                "kmoon@tdd.com.br",
-                "Wembley - UK",
-                "Male"
-        );
 
         // When
         Person actual = service.createPerson(person);
