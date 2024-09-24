45a879a2c68d2c7aade47739a3ef04d9444813fe
Hebert <hebertLandim007@outlook.com>
2024-09-24 16:07:27 -0300

    Checking if the Created Person Contains a FirstName

--- S05_JUnit5andTDD/src/test/java/br/com/tdd/PersonServiceTest.java
+++ S05_JUnit5andTDD/src/test/java/br/com/tdd/PersonServiceTest.java
@@ -31,4 +31,26 @@ public class PersonServiceTest {
         //Then
         assertNotNull(actual, () -> "The createPerson() should not have returned null!");
     }
+
+    @DisplayName("When Create a Person with Success Should Contains FirstName in Returned Person Object")
+    @Test
+    void testCreatePerson_WhenSuccess_ShouldContainsFirstNameInReturnedPersonObject() {
+
+        // Given
+        IPersonService service = new PersonService();
+
+        Person person = new Person(
+                "Keith",
+                "Moon",
+                "kmoon@tdd.com.br",
+                "Wembley - UK",
+                "Male"
+        );
+
+        // When
+        Person actual = service.createPerson(person);
+
+        //Then
+        assertEquals(person.getFirstName(), actual.getFirstName() , () -> "The firstName is Different!");
+    }
 }
