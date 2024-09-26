0559bad473993030a5f1c7b511a39867189dc27a
Hebert <hebertLandim007@outlook.com>
2024-09-26 13:07:53 -0300

    Mocking Lists

---
+++ S06_MockitosFirstSteps/src/test/java/br/com/mockito/business/ListTest.java
@@ -0,0 +1,49 @@
+package br.com.mockito.business;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.*;
+
+public class ListTest {
+
+    @Test
+    void testMockingList_When_SizeIsCalled_ShouldReturnMultipleValues() {
+
+        // Given
+        List<?> list = mock(List.class);
+        when(list.size()).thenReturn(10).thenReturn(20);
+
+        // When & Then
+        assertEquals(10, list.size());
+        assertEquals(20, list.size());
+        assertEquals(20, list.size());
+    }
+
+    @Test
+    void testMockingList_When_GetIsCalled_ShouldReturnUser() {
+
+        // Given
+        var list = mock(List.class);
+        when(list.get(0)).thenReturn("User");
+
+        // When & Then
+        assertEquals("User", list.get(0));
+        assertNull(list.get(1));
+    }
+
+    @Test
+    void testMockingList_When_GetIsCalledWithArgumentMatcher_ShouldReturnUser() {
+
+        // Given
+        var list = mock(List.class);
+        when(list.get(anyInt())).thenReturn("User");
+
+        // When & Then
+        assertEquals("User", list.get(anyInt()));
+        assertEquals("User", list.get(anyInt()));
+    }
+}
