d757f77189c6db85dbdb0348ddf753333cf545b4
hebert <hebertLandim007@outlook.com>
2024-10-17 17:08:25 -0300

    Testing a REST API in Spring Boot 3 - Repositories and Services Layer

---
+++ S11_TestingServices/src/test/java/br/com/spring/Rest_With_Spring/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.Rest_With_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
