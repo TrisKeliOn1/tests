02ea7ad47d715377867a34b35f8ed4c8ae6ca11e
hebert <hebertLandim007@outlook.com>
2024-10-19 21:27:33 -0300

    Testing a REST API - Integration Tests with Testcontainers

---
+++ S13_IntegrationTests/src/test/java/br/com/spring/Rest_With_Spring/config/TestConfigs.java
@@ -0,0 +1,7 @@
+package br.com.spring.Rest_With_Spring.config;
+
+public class TestConfigs {
+
+    public static final int SERVER_PORT = 8888;
+    public static final String CONTENT_TYPE_JSON = "application/json";
+}
